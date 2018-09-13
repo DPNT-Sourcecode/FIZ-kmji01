280ded6b8eaa606be12e7cf3c68479557ec7f21c
sbx_user1060 <sbx_user1060@ip-10-128-2-28.eu-west-2.compute.internal>
2018-09-13 11:48:59 +0000

    Thu Sep 13 11:48:59 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -4,6 +4,9 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class FizzBuzzSolution {
 
+	static final String FIZZ = "fizz";
+	static final String BUZZ = "buzz";
+	
     public String fizzBuzz(Integer number) {
         return "";
     }
