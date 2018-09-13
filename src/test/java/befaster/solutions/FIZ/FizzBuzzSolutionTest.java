3970356455e68405c18a05f6d8764603c5e76295
sbx_user1060 <sbx_user1060@ip-10-128-2-28.eu-west-2.compute.internal>
2018-09-13 12:26:20 +0000

    Thu Sep 13 12:26:20 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -15,8 +15,8 @@ public class FizzBuzzSolutionTest {
 
 	@Test
 	public void testFizz() {
-		assertEquals(189, new FizzBuzzSolution().fizzBuzz(Integer.valueOf(189)));
-		assertEquals(198, new FizzBuzzSolution().fizzBuzz(Integer.valueOf(198)));
+		assertEquals("189", new FizzBuzzSolution().fizzBuzz(Integer.valueOf(189)));
+		assertEquals("198", new FizzBuzzSolution().fizzBuzz(Integer.valueOf(198)));
 		assertEquals("fizz", new FizzBuzzSolution().fizzBuzz(Integer.valueOf(13)));
 	}
 	
@@ -30,7 +30,7 @@ public class FizzBuzzSolutionTest {
 	@Test
 	public void testBuzz() {
 		assertEquals("buzz", new FizzBuzzSolution().fizzBuzz(Integer.valueOf(25)));
-		assertEquals("buzz", new FizzBuzzSolution().fizzBuzz(Integer.valueOf(10)));
+		assertEquals("10", new FizzBuzzSolution().fizzBuzz(Integer.valueOf(10)));
 	}
 	
 	@Test
@@ -40,8 +40,10 @@ public class FizzBuzzSolutionTest {
 	
 	@Test
 	public void testFizzBuzz() {
-		assertEquals("fizz buzz", new FizzBuzzSolution().fizzBuzz(Integer.valueOf(15)));
-		assertEquals("fizz buzz", new FizzBuzzSolution().fizzBuzz(Integer.valueOf(35)));
+		// 135 is div by 3 and 5
+		assertEquals("fizz buzz", new FizzBuzzSolution().fizzBuzz(Integer.valueOf(135)));
+		// 35 has a 3 in it but not div by 3
+		assertEquals("buzz", new FizzBuzzSolution().fizzBuzz(Integer.valueOf(35)));
 	}
 
 	@Test
