684ed1dfbd7e4d83979145c07acf878e23b3c682
sbx_user1060 <sbx_user1060@ip-10-128-2-28.eu-west-2.compute.internal>
2018-09-13 11:13:14 +0000

    Thu Sep 13 11:13:14 UTC 2018

--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -4,6 +4,11 @@ public class HelloSolution {
 	
 	static String HELLO_PREFIX  = "Hello,";
     public String hello(String friendName) {
-        return friendName != null && !friendName.trim().isEmpty() ? HELLO_PREFIX + " " + friendName.trim() "!" : HELLO_PREFIX;
+    	
+    	final StringBuilder sb = new StringBuilder(HELLO_PREFIX);
+    	if (friendName == null || friendName.trim().isEmpty())
+    	{
+    		sb.append(' ').append(friendName.trim()).append("!")).toString()
+    	}
     }
 }
