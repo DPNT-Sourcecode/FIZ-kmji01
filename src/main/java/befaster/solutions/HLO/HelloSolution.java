09089937d155f4968e325223a38d40bf49776bdc
sbx_user1060 <sbx_user1060@ip-10-128-2-28.eu-west-2.compute.internal>
2018-09-13 11:10:14 +0000

    Thu Sep 13 11:10:14 UTC 2018

--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -2,8 +2,8 @@ package befaster.solutions.HLO;
 
 public class HelloSolution {
 	
-	static String HELLO_PREFIX  = "Hello";
+	static String HELLO_PREFIX  = "Hello,";
     public String hello(String friendName) {
-        return "Hello, World!";
+        return friendName != null && !friendName.trim().isEmpty() ? HELLO_PREFIX + " " + friendName.trim() "!" : HELLO_PREFIX;
     }
 }
