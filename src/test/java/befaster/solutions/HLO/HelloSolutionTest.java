6bcd838efa23b020cfe8ce0f62a9429eb09bc892
sbx_user1060 <sbx_user1060@ip-10-128-2-28.eu-west-2.compute.internal>
2018-09-13 10:49:37 +0000

    Thu Sep 13 10:49:37 UTC 2018

---
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -0,0 +1,14 @@
+package befaster.solutions.HLO;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloSolutionTest {
+
+	@Test
+	public void testHello() {
+		fail("Not yet implemented");
+	}
+
+}
