9a7531d995e6c8aa6b6f8bb9f31f347f70b18cf3
sbx_user1060 <sbx_user1060@ip-10-128-2-28.eu-west-2.compute.internal>
2018-09-13 11:52:00 +0000

    Thu Sep 13 11:52:00 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -1,14 +1,29 @@
 package befaster.solutions.FIZ;
 
-import befaster.runner.SolutionNotImplementedException;
-
 public class FizzBuzzSolution {
 
 	static final String FIZZ = "fizz";
 	static final String BUZZ = "buzz";
 	
-    public String fizzBuzz(Integer number) {
-        return "";
+    public String fizzBuzz(final Integer number) {
+    	final int value = number.intValue();
+    	final boolean isDivThree = value%3 == 0;
+    	final boolean isDivFive = value%5 == 0;
+    	
+    	if (isDivThree || isDivFive)
+    	{
+    		final StringBuilder sb = new StringBuilder(9);
+    		if (isDivThree){
+    			sb.append(FIZZ);
+    			sb.append(' ');
+    		}
+    		if (isDivFive) {
+    			sb.append(BUZZ);
+    			sb.append(' ');
+    		}
+    		return sb.toString().trim();
+    	}
+    	return String.valueOf(value);
     }
 
 }
--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -20,12 +20,17 @@ public class FizzBuzzSolutionTest {
 	
 	@Test
 	public void testBuzz() {
-		assertEquals("buzz", new FizzBuzzSolution().fizzBuzz(Integer.valueOf(645)));
+		assertEquals("buzz", new FizzBuzzSolution().fizzBuzz(Integer.valueOf(25)));
 	}
 	
 	@Test
 	public void testFizzBuzz() {
 		assertEquals("fizz buzz", new FizzBuzzSolution().fizzBuzz(Integer.valueOf(15)));
+	}
+
+	@Test
+	public void testFizzBuzzNone() {
+		assertEquals("98", new FizzBuzzSolution().fizzBuzz(Integer.valueOf(98)));
 	}
 
 }
